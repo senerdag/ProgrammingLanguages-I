42b1b5cfedf6888bbc849d488fe0fe4e3e5c41de
Şener Dağ <79213168+senerdag@users.noreply.github.com>
2024-01-08 19:36:42 +0300

    Create PL1-exercises.c

---
+++ week07/PL1-exercises.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+/*  First Question
+int a,b;
+printf("Enter 2 number: ");
+scanf("%d",&a);
+scanf("%d",&b);
+printf("%d",a+b);
+*/
+
+/* Second Question
+int a,b,c;
+printf("Enter 2 number: ");
+scanf("%d",&a);
+scanf("%d",&b);
+c=a;
+a=b;
+b=c;
+printf("%d, %d",a,b);
+*/
+
+/* Third Question
+float a,b;
+printf("Sirasiyla vize ve final notunu girin: ");
+scanf("%f",&a);
+scanf("%f",&b);
+if(((a*40)/100)+((b*60)/100)>=50)
+{
+    printf("Gectinizz!!!");
+}
+else
+{
+    printf("Kaldiniz!");
+}
+*/
+
+/* Fourth Question
+int a,b,c;
+printf("Yari capi giriniz: ");
+scanf("%d",&c);
+printf("Islemi girini: ");
+scanf("%d",&a);
+if(a==1)
+{
+    printf("%d",2*3*c);
+}
+else if (a==2)
+{
+    printf("%d",3*c*c);
+}
+else
+{
+    printf("Gecerli bi mod giriniz.");
+}
+*/
+
+
+
+
+
+
+
+
+
+    return 0;
+
+}
